3133aa1ed83d2b571c2d609e51846f7b954d2ab0
Tony Philip <tonyphilip@gmail.com>
2015-04-05 11:35:02 -0700

    Annotation added

--- CommandLineTemplateC/CommandLineTemplateC/main.cpp
+++ CommandLineTemplateC/CommandLineTemplateC/main.cpp
@@ -7,15 +7,15 @@
 //
 #include <iostream>// works with inputs and outputs
 
-using namespace std;
+using namespace std;  //Load standard out controls for cout = console out
 
 int main(int argc,char* argv[])
 {
-    if (argc==1)
+    if (argc==1) //Check for at least one arg.  If none are found display usage statement
         cout << "Usage; ./program_name arg1" << endl;
     else
     {
-        for(int i=1;i < argc; i++)
+        for(int i=1; i < argc; i++) //For each argument passed with the command do
             cout << argv[i] << endl;
     }
     return 0;
